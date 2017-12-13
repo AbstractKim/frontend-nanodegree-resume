aaab0c7b3937c323ce1a5affe65d6366c3cdaa31
Peter Beomseok Kim <b.kim@spacepalm.com>
2017-12-13 15:11:17 -0500

    quiz:string.replace()

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,5 +2,14 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- $("#main").append("Peter Beomseok Kim");
+
+ var email = "peter@udacity.com"
+ var newEmail = email.replace("udacity", "gmail");
+ console.log(email);
+ console.log(newEmail);
+
+var awesomeThought = "I am Peter and awesome";
+var funThought = awesomeThought.replace("awesome", "fun");
+
+ $("#main").append(funThought);
 
