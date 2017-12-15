00890578b4367dd8ac787fe27737f282c6978db0
Peter Beomseok Kim <b.kim@spacepalm.com>
2017-12-15 00:49:52 -0500

    13.string mnipulation raw code

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -32,16 +32,29 @@ var sampleArray = [0,0,7];
 
 var incrementLastArrayElement = function(_array){
 	var newArray = [];
-	/*
-	for(let i=0; i < _array.length; i++){
-		newArray.push(_array[i]);
-	}
-	return newArray;
-	*/
-	_array.forEach(function(i){
-		newArray.push(i);
-	});
+	
+	newArray = _array.slice(0); //relpace foreach
+	newArray.push(newArray.pop() + 1);
 	return newArray;
 }
 
 console.log(incrementLastArrayElement(sampleArray));
+
+
+var name = "AlbErt EINstEiN";
+
+function nameChanger(oldName){
+	//1.make lowercase
+	lowerCaseName = oldName.toLowerCase();
+	//2. split by " " to first name and last name
+	nameArray = lowerCaseName.split(" ");
+	//3. first char of first name changes to capital
+	firstName = nameArray[0].slice(0,1).toUpperCase() + nameArray[0].slice(1);
+	//4. lastName changes to Upper Case 
+	lastName = nameArray[1];
+	finalName = firstName + " " + lastName.toUpperCase();
+
+	return finalName;
+};
+
+console.log(nameChanger(name));
